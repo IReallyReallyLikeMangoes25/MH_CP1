bdd283d61ed4712cdca72e7ea75a5896695544e9
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-08-20 10:47:31 -0600

    I did a name input.

---
+++ Practices/hello_world.py
@@ -0,0 +1,6 @@
+# MH 2nd hello world practice
+
+name = input("Hi! What's your name? ")
+print("Nice to meet you", name, ", I am a computer.")
+
+
---
+++ notes/Inputs.py
@@ -0,0 +1,12 @@
+# MH 2nd Input/output notes
+
+color = input("Give me a color:")
+print(color, " is a sucky color, why would you even say that.")
+
+#other option
+
+print("Tell me another color:")
+color_two = input()
+print(color_two, " is a much better color, thank you.")
+
+#Pick one method and use it forever, do not change between methods.
