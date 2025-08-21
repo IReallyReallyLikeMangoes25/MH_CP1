5e3423a068cf053eaefa1b5f0ce4f9e9400bddda
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-08-21 10:09:44 -0600

    did my seasons started

---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+#MH 2nd seasons
+
+name = input("Hey, what's your last name? ")
+season = input("And what's your favorite season? Hsi")
+print("Nice to meet you", name, "san, I like", season, "too!")
