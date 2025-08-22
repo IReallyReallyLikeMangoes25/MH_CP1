9ba9ccb78c4b201fbfbcfd0d23001c0f909cdc0d
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-08-22 10:51:17 -0600

    Working on who are you project

---
+++ projects/who_are_you.py
@@ -0,0 +1,8 @@
+# MH 2nd Who are you project
+print("Hello! Welcome to Mirai's first coding project of the semster! Please answer a few questions:")
+name = input("What is your name: ")
+age = input("What is your age: ")
+fav_color = input("What is your favorite color: ")
+print("Welcome", name, ", my favorite color is also", fav_color, ", though I am definitely not", age, ".")
+
+# for advanced: creat list to store data that hs been input before, a loop after name to check if name has been input before, if it has display that persons information
