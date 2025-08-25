85817361de8bfcfc4052085fb77c545e626635e3
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-08-25 10:37:37 -0600

    Finished my who are you project.

--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -1,8 +1,18 @@
 # MH 2nd Who are you project
-print("Hello! Welcome to Mirai's first coding project of the semster! Please answer a few questions:")
-name = input("What is your name: ")
-age = input("What is your age: ")
-fav_color = input("What is your favorite color: ")
-print("Welcome", name, ", my favorite color is also", fav_color, ", though I am definitely not", age, ".")
+info = {}
+num = 0
+
+while num == 0:
+    print("Hello! Welcome to Mirai's first coding project of the semster! Please answer a few questions:")
+    name = input("What is your name: ")
+    if name in info:
+        print("Welcome back,", name)
+        print(info[name])
+    else:
+        age = input("What is your age: ")
+        fav_color = input("What is your favorite color: ")
+        print("Welcome", name, ", my favorite color is also", fav_color, ", though I am definitely not", age, ".")
+        info[name.title()] = age, fav_color
+        print(info)
 
 # for advanced: creat list to store data that hs been input before, a loop after name to check if name has been input before, if it has display that persons information
