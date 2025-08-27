1a7ec0560f32fa3497f82bfb79be5f272ab8c33a
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-08-27 10:50:49 -0600

    Began integer and float notes

---
+++ notes/numbers.py
@@ -0,0 +1,17 @@
+# MH 2nd Integer and Floats Notes
+
+#integer
+age = 172985698573987398719832
+temp = 85
+attention = -8
+spoons = 4
+
+# float
+pi = 3.14
+pounds = 5.6
+emotional_damage = .99
+patience = -.50
+
+#MATH
+# Parenthesis, exponents, multiplication, division, integer division, modulo, addition, subtraction.
+print(8/4 + 7 - (8+2)%4**2)
