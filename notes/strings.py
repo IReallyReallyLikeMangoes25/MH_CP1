ca250b51a3f3008c57cbd726bc46ce02e159063a
IReallyReallyLikeMangoes <mirai.hsiao@ucas-edu.net>
2025-09-02 10:51:10 -0600

    Almost done with strings notes

--- notes/strings.py
+++ notes/strings.py
@@ -1,2 +1,25 @@
 # MH 2nd strings notes
 
+cool_string = "This is an example of a string."
+another_string = 'This is also a string.'
+
+# If I need to use " or '
+
+name = "La' Rose"
+best = '"the Return of the King" is the best book ever!'
+
+length = len(name)
+print("Your name has", length, "characters in it.")
+
+#escape character usage
+print('\tWill says \n"don\'t die"')
+
+#concatenate
+
+the_best_string = cool_string + " " + another_string
+print(the_best_string)
+
+#slice/index
+the_best_string[4, 7]
+
+
