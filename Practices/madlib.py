8636ff277d1f57c30c901db80435eaab2e02af17
IReallyReallyLikeMangoes <mirai.hsiao@ucas-edu.net>
2025-09-03 10:45:13 -0600

    I made a madlib

---
+++ Practices/madlib.py
@@ -0,0 +1,17 @@
+# MH 2nd madlib practice
+
+adjective_1 = input("Give me an adjective: ")
+adjective_2 = input("Give me an adjective: ")
+bird = input("Give me a type of bird: ")
+room = input("Give me a room in a house: ")
+verb_past = input("Give me a past-tense verb: ")
+verb_1 = input("Give me an verb: ")
+name = input("Give me the name of a relative: ")
+noun_1 = input("Give me a noun: ")
+liquid = input("Give me a liquid: ")
+verb_ing = input("Give me a verb ending in -ing: ")
+body_part = input("Give me a body part (plural): ")
+plural_noun = input("Give me a plural noun: ")
+noun_2 = input("Give me a noun: ")
+
+print("it was a funny " + adjective_1 + ", cold November day. I woke up to the " + adjective_2 + " smell of " + bird + " roasting in the " + room + " downstairs. I " + verb_past + " down the stairs to see if I could help " + verb_1 +  ' the dinner. My mom said, "See if ' + name + ' needs a fresh ' + noun_1 + '." So I carried a tray of glasses full of ' + liquid + " into the " + verb_ing + " room. When I got there, I couldn't believe my " + body_part + "! There were " + plural_noun + " " + verb_ing + " on the " + noun_2 + "!" )
