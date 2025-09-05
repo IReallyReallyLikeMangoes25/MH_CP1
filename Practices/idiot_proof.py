fffc23a40fef17161281f510f2c995f748a5a131
IReallyReallyLikeMangoes <mirai.hsiao@ucas-edu.net>
2025-09-05 10:49:21 -0600

    Idiot proof

--- Practices/idiot_proof.py
+++ Practices/idiot_proof.py
@@ -1,3 +1,13 @@
 # MH 2nd idiot proof practice
 
-name = input()
+name = input("What if your full name? ").title().strip()
+number = input("What is your phone number? Please put dashes between every section. ").replace("-", " ")
+if len(number) < 12:
+    print("Invalid number, please try again.")
+
+    number = input("What is your phone number? Please put dashes between every section. ").replace("-", " ")
+
+gpa = float(input("What is your GPA? "))
+rounded_gpa = round(gpa, 1)
+
+print(name, number, rounded_gpa)
