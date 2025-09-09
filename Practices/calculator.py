b172ffa3e674d23630f272ce7d7ca92e79dd3597
IReallyReallyLikeMangoes <mirai.hsiao@ucas-edu.net>
2025-09-09 10:33:03 -0600

    Finished a basic calculator

--- Practices/calculator.py
+++ Practices/calculator.py
@@ -7,7 +7,7 @@ while True:
     sub = (num_1 - num_2)
     div = (num_1 / num_2)
     mult = (num_1 * num_2)
-    operator = int(input("Would you like to 1. add, 2. subtract, 3. multiply, or 4. divide?"))
+    operator = int(input("Would you like to 1. add, 2. subtract, 3. multiply, or 4. divide? (Please answer with 1/2/3/4) "))
 
 
     if operator == 1:
@@ -18,6 +18,11 @@ while True:
         print(f"{num_1} * {num_2} = {mult}")
     elif operator == 4:
         print(f"{num_1} / {num_2} = {div}")
+
+    again = int(input("Do you want to run another operation? 1. Yes 2. No (Please answer with 1/2) "))
+
+    if again == 2:
+        break
     
 
     
