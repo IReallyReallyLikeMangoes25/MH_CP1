60157b22027795f3513b138ff8c69f88b3b19ef9
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-09-19 10:39:40 -0600

    Took conditionals notes

---
+++ notes/conditionals.py
@@ -0,0 +1,28 @@
+# MH 2nd Conditionals notes
+
+import random
+monster_hp = 30
+dmg_mod = 2
+atk_bonus = 3
+roll = random.randint(1, 20)
+player_hp = 25
+
+if roll == 20:
+    print(f"You rolled a crit! Double your damage! {roll}")
+    attack = random.randint(1, 8) + random.randint(1, 8) + dmg_mod
+    monster_hp -= attack
+    print(f"You did {attack} damage to the monster!")
+elif roll + atk_bonus > 10:
+    attack = random.randint(1, 8) + dmg_mod
+    monster_hp -= attack
+    print(f"You did {attack} damage to the monster!")
+elif roll <= 10:
+    if roll == 1:
+        print(f"You rolled a critical failure! The moster gets a free attack!")
+        damage = random.randint(1, 10) + 2
+        player_hp -= damage
+        print(f"You took {damage} damage, health remaining: {player_hp}")
+else:
+    print(f"you missed! {roll}")
+
+print("Your turn is over.")
