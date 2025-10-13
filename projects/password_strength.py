9610803866576733b9ad0f9b1e3e3702c508981c
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-10-13 10:50:30 -0600

    working on  a password strength checker

--- projects/password_strength.py
+++ projects/password_strength.py
@@ -15,13 +15,13 @@ while True:
     # LOOP ONE
         # if password is at least 8 characters add one point
         # else add no points and add "You must have at least 8 characters" to list of outputs
-    if password.len() >= 8:
-            point += 1
+    if len(password) >= 8:
+            points += 1
 
     else: 
-            outputs.extend("At least 8 characters")
+            outputs.append("At least 8 characters")
 
-    # LOOP TWO
+     # LOOP TWO
         # if password contains at least one uppercase letter add one point
         # else add no points and add "You must have at least one uppercase letter" to list of outputs
     for i in password:
@@ -30,7 +30,8 @@ while True:
             break
 
         else:
-            outputs.extend("At least one uppercase letter")
+            outputs.append("At least one uppercase letter")
+            break
 
     # LOOP THREE
         # if password contains at least one lowercase letter add one point
@@ -41,7 +42,8 @@ while True:
             break
          
         else:
-            outputs.extend("at least one lowercase letter")
+            outputs.append("at least one lowercase letter")
+            break
 
     # LOOP FOUR
         # if password contains at least one number add one point
@@ -52,7 +54,8 @@ while True:
             break
             
         else:
-            outputs.extend("At least one number")
+            outputs.append("At least one number")
+            break
     # LOOP FIVE
         # if password contains at least one special character add one point
         # else add no points and add "You must have at least one special character" to list of outputs   
@@ -60,11 +63,11 @@ while True:
         points += 1
 
     else:
-        outputs.extend("At least one special character")
+        outputs.append("At least one special character")
 
     
     # disply points
-    print(points)
+    print(f"Your password strength is {points}/5 points.")
     # LOOP SIX
         # if points = 5 stop running code
         # else go back to start of program and display changes that need to be made
@@ -73,4 +76,3 @@ while True:
     
     else:
         print(outputs)
-         
