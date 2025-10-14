3352de696831953b79affd139651724eb67cfd45
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-10-13 21:09:11 -0600

    Finished password strength checker

--- projects/password_strength.py
+++ projects/password_strength.py
@@ -6,12 +6,17 @@ outputs = ["Password still needs:"]
 points = 0
 # special characters
 spec_chars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", "|", ";", ":", ",", ".", "<", ">", "?"]
+upper = False
+lower = False
+num = False
 
 # while loop that runs until user has a complete password
 while True:
     # asks user for password
-    password = str(input("What is your password?"))
+    password = str(input("What is your password?\n"))
     print(password)
+    points = 0
+    outputs = ["Password still needs:"]
     # LOOP ONE
         # if password is at least 8 characters add one point
         # else add no points and add "You must have at least 8 characters" to list of outputs
@@ -27,11 +32,11 @@ while True:
     for i in password:
         if i.isupper():
             points += 1
+            upper = True
             break
 
-        else:
-            outputs.append("At least one uppercase letter")
-            break
+    if upper == False:
+        outputs.append("At least one capital letter")
 
     # LOOP THREE
         # if password contains at least one lowercase letter add one point
@@ -39,32 +44,38 @@ while True:
     for i in password:
         if i.islower():
             points += 1
+            lower = True
             break
          
-        else:
-            outputs.append("at least one lowercase letter")
-            break
+    if lower == False:
+        outputs.append("At least one lowercase letter")
 
     # LOOP FOUR
         # if password contains at least one number add one point
         # else add no points and add "You must have at least one number" to list of outputs
     for i in password:
-        if i.isalnum():
+        if i.isdigit():
             points += 1
+            num = True
             break
             
-        else:
-            outputs.append("At least one number")
-            break
+    if num == False:
+        outputs.append("At least one number")
     # LOOP FIVE
         # if password contains at least one special character add one point
         # else add no points and add "You must have at least one special character" to list of outputs   
-    if spec_chars in password:
-        points += 1
-
-    else:
+    password = list(password)
+    
+    for i in password:
+        if i in spec_chars:
+            points += 1
+            spec = True
+            break
+    if spec == False:
         outputs.append("At least one special character")
 
+    
+    password = str(password)
     
     # disply points
     print(f"Your password strength is {points}/5 points.")
