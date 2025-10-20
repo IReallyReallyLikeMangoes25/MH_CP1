8a8bfd86a605dc4f49f9a91dbb6c3671d58d4a7b
IReallyReallyLikeMangoes <mirai.hsiao@ucas-edu.net>
2025-10-20 10:51:12 -0600

    functions notes

--- notes/functions.py
+++ notes/functions.py
@@ -1,1 +1,13 @@
-# MH 2nd Functions notes
+# MH 2nd period Functions notes
+
+def addNum(x, y):
+    return x + y
+
+print(addNum(5, 5))
+
+num = addNum(addNum(-1347, 1234), addNum(5, 50))
+print(num)
+
+while num < addNum(9.2342, 42):
+    print("duck")
+    num += 1
