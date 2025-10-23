46cbbc8e2cc782c7daea1c1f2a8f82e92e584b85
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-10-22 23:08:11 -0600

    Finished caeser cipher...... Finally

--- Practices/caeser_cipher.py
+++ Practices/caeser_cipher.py
@@ -4,51 +4,76 @@
 cipher = str(input("Would you like to encode a message, or decode a message?\n"))
 cipher.lower()
 cipher.strip()
-message = str(input("What message would you like to encode?\n"))
 
 # function to encode a message
 def encode(message):
     # takes message and goes letter by letter shifting them by an inputed amount
     shift = int(input("How many letters should it be shifted by?\n"))
     coded = ""
-    # if it's a space it leaves it
-    # if it's punctiuation it leaves it
+    # checks if the shift goes past z
+    # if it does it goes back to a
     # saves every letter in a new string
     for i in message:
         if i.isalpha():
-            num = ord(i)
-            num += shift
-            new = chr(num)
-            coded += new
+            if i.isupper():
+                num = ord(i) - 65
+                shift = shift % 26
+                if (num + shift) > 25:
+                    num -= 26
+                num += shift
+                new = chr(num + 65)
+                coded += new
+            else:
+                num = ord(i) - 97
+                shift = shift % 26
+                if (num + shift) > 25:
+                    num -= 26
+                num += shift
+                new = chr(num + 97)
+                coded += new
         else:
             coded += i
-
+    # adds on every letter to an empty string
     print(coded)
     # returns the message but shifted
 
 # function to decode a message
 def decode(message):
-    # takes in an encoded message and goes letter by letter shifting them by and inputed amount
+    # takes in an encoded message and goes letter by letter shifting them by an inputed amount
     shift  = int(input("How many places should we shift it back by\n"))
     decoded = ""
-    # if it's a space it leaves it
-    # if it's punctuation it leaves it
+    # checks if after shifting the letter goes past a
+    # if it does it goes back to z
     # saves every letter in a new string
-    # returns the message decoded
     for i in message:
         if i.isalpha():
-            num = ord(i)
-            num -= shift
-            new = chr(num)
-            decoded += new
+            if i.isupper():
+                num = ord(i) - 65
+                shift = shift % 26
+                if (num - shift) < 0:
+                    num += 26
+                num -= shift
+                new = chr(num + 65)
+                decoded += new
+            else:
+                num = ord(i) - 97
+                shift = shift % 26
+                if (num - shift) < 0:
+                    num += 26
+                num -= shift
+                new = chr(num + 97)
+                decoded += new
         else:
             decoded += i
-    
+    # adds on every letter to an empty string
     print(decoded)
+    # returns message but decoded
 
 # if the user wants to encode, run the encode function
 # if the user wants to decode, run the decode function
-if cipher == "encode":
-    encode(message)
+if cipher == "decode":
+    decode(str(input("What message would you like to decode?\n")))
+elif cipher == "encode":
+    encode(str(input("What message would you like to encode?\n")))
 else:
-    decode(message)
+    print("That is not a valid input.")
