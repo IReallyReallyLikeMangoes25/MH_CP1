440fdf90e0ccdacaa9d2b56096f5aac4445dec13
IReallyReallyLikeMangoes <mirai.hsiao@ucas-edu.net>
2025-10-27 10:51:01 -0600

    working on combat

--- Practices/combat.py
+++ Practices/combat.py
@@ -1,5 +1,6 @@
 # MH 2nd combat practice
 import time
+import random
 
 name = ("Hello young trainer! What is your name?\n")
 starter = str(input(f"Hello {name}, nice to meet you. I am Professor Oak, please choose a starter pokemon:\n1. Bulbasaur\n2. Squirtle\n3. Charmander\n"))
@@ -23,89 +24,168 @@ rspeed = 0
 if starter == 1:
     starter = "Bulbasaur"
     hp = 45
-    atk = 49
-    defense = 49
+    atk = 50
+    defense = 50
     spec_atk = 65
     spec_def = 65
     speed = 45
 
     rival = "Charmander"
-    rhp = 39
-    ratk = 52
-    rdefense = 43
+    rhp = 40
+    ratk = 50
+    rdefense = 45
     rspec_atk = 60
     rspec_def = 50
     rspeed = 65
 
 elif starter == 2:
     starter = "Squirtle"
-    hp = 44
-    atk = 48
+    hp = 45
+    atk = 50
     defense = 65
     spec_atk = 50
-    spec_def = 64
-    speed = 43
+    spec_def = 65
+    speed = 45
 
     rival = "Bulbasaur"
     rhp = 45
-    ratk = 49
-    rdefense = 49
+    ratk = 50
+    rdefense = 50
     rspec_atk = 65
     rspec_def = 65
     rspeed = 45
 
 else:
     starter = "Charmander"
-    hp = 39
-    atk = 52
-    defense = 43
+    hp = 40
+    atk = 50
+    defense = 45
     spec_atk = 60
     spec_def = 50
     speed = 65
 
     rival = "Squirtle"
-    rhp = 44
-    ratk = 48
+    rhp = 45
+    ratk = 50
     rdefense = 65
     rspec_atk = 50
-    rspec_def = 64
-    rspeed = 43
+    rspec_def = 65
+    rspeed = 45
 
-def your_turn(starter, hp, atk, defense, spec_atk, spec_def, speed, rival, rhp, ratk, rdefense, rspec_atk, rspec_def, rspeed):
+def your_turn(action, starter, atk, rdefense, spec_atk, rspec_def):
     potions = 1
     damage = 0
-    if starter = "Bulbasaur"
-        action = print(int(input(f"What should Bulbasaur do?\n1. Tackle\n2. Growl\n3. Vine Whip\n4. Potion (1 left)")))
+    atk_debuff = 0
+    def_debuff = 0
+    boost = 0
+    if starter == "Bulbasaur":
         if action == 1:
-            print("Bulbasaur used Vine whip!")
-            damage += 
+            print("Bulbasaur used Tackle!")
+            damage += ((atk + 40) / 5) - (rdefense / 15)
+            return damage
         elif action == 2:
-            pass
+            print("Bulbasaur used growl!")
+            atk_debuff += 10
+            return atk_debuff
         elif action == 3:
-            pass
+            print("Bulbasuar used vine Whip!")
+            damage += ((atk + 45) / 5) - (rdefense / 5)
         else:
-            pass
+            if potions == 1:
+                print("Used a potion. 0 left.")
+                potions -= 1
+                boost += 15
+                return boost
+            else:
+                print("You are out of potions.")
     
-def rival_turn(rhp, ratk, rdefense, rspec_atk, rspec_def, rspeed):
-    pass
+    elif starter == "Squirtle":
+        if action == 1:
+            print("Squirtle used Tackle!")
+            damage += ((atk + 40) / 5) - (rdefense / 15)
+            return damage
+        elif action == 2:
+            print("Squirtle used Tail Whip!")
+            def_debuff += 15
+            return def_debuff
+        elif action == 3:
+            print("Squirtle used Water Gun!")
+            damage += ((spec_atk + 40) / 5) - (rspec_def / 5)
+        else:
+            if potions == 1:
+                print("Used a potion. 0 left.")
+                potions -= 1
+                boost += 15
+                return boost
+            else:
+                print("You are out of potions.")
+    
+    else:
+        if action == 1:
+            print("Charmander used Tackle!")
+            damage += ((atk + 40) / 5) - (rdefense / 15)
+            return damage
+        elif action == 2:
+            print("Charmander used Growl!")
+            atk_debuff += 15
+            return atk_debuff
+        elif action == 3:
+            print("Charmander used Ember!")
+            damage += ((spec_atk + 40) / 5) - (rdefense / 5)
+        else:
+            if potions == 1:
+                print("Used a potion. 0 left.")
+                potions -= 1
+                boost += 15
+                return boost
+            else:
+                print("You are out of potions.")
+
+
+    
+def rival_turn(action, starter, hp, atk, defense, spec_atk, spec_def, speed, rival, rhp, ratk, rdefense, rspec_atk, rspec_def, rspeed):
+    potions = 1
+    damage = 0
+    atk_debuff = 0
+    def_debuff = 0
+    boost = 0
+    if starter == "Bulbasaur":
+        if action == 1:
+            print("Rivals Bulbasaur used Tackle!")
+            damage += ((atk + 40) / 5) - (rdefense / 15)
+            return damage
+        elif action == 2:
+            print("Rivals Bulbasaur used Growl!")
+            atk_debuff += 10
+            return atk_debuff
+        elif action == 3:
+            print("Rivals Bulbasaur used Vine Whip!")
+            damage += ((atk + 45) / 5) - (rdefense / 5)
+        else:
+    
+    elif starter == "Squirtle":
+        pass
+    else:
+        pass
+
 
 def turns_1():
     while hp > 0 or rhp > 0 :
-    if hp < 0:
-        winner = "Rival"
-    elif rhp < 0:
-        winner = "You"
-    else:
-
+        if hp < 0:
+            winner = "Rival"
+        elif rhp < 0:
+            winner = "You"
+        else:
+            pass
 
 def turns_2():
     while hp > 0 or rhp > 0:
-    if hp < 0:
-        winner = "Rival"
-    elif rhp < 0:
-        winner = "You"
-    else:
-
+        if hp < 0:
+            winner = "Rival"
+        elif rhp < 0:
+            winner = "You"
+        else:
+            pass
 
 
 print(f"Great! I think {starter} is the perfect choice for you. It's stats are:\nHP = {hp}\nSpeed = {speed}\nAttack = {atk}\nDefense = {defense}\nSpecial attack = {spec_atk}\nSpecial defense = {spec_def}")
@@ -116,10 +196,9 @@ print(f"Go {rival}!")
 
 if speed > rspeed:
     print(f"{starter} goes first!")
-    first = "player"
+
 else:
     print(f"{rival} goes first!")
-    first = "rival"
     
     
     
