d4baaf055f37f19c6d6a2bca2a81b4d788ea4e79
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-11-03 21:17:43 -0700

    Working on a maze generator

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -3,30 +3,13 @@
 import turtle
 import random
 
-# six empty rows/collumns
-def setup():
-    rows = [
-        []
-        []
-        []
-        []
-        []
-        []
-    ]
-
-    cols = [
-        []
-        []
-        []
-        []
-        []
-        []
-    ]
-
-def randomnation(rows):
-    for row in rows:
-
-
-
-
-# turtle goes space by space in collumns/rows adding lines
+# six squares
+# turtle goes sqaure by square and goes to every side of every square and randomly decides if that side will be white or black:
+# turtle goes to square
+# turtle decides if side 1 is white/black
+# turtle goes to next side
+# decides if side 2 is white or black
+# turtle goes to next side
+# turtle decides if side 3 
+# turtle draws a border around all squares
+# turtle draws line through maze- that is the correct path.
