4cf6fb5114e2958443169ee5cc672750a1ad50ae
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-12-01 10:36:54 -0700

    working on final project

---
+++ projects/final_project.py
@@ -0,0 +1,52 @@
+# MH 2nd final project
+
+# play: ask user "Do you want to play or quit?"
+
+# STATS/MOVES:
+    # player:
+        # health - 7
+        # speed - 
+        # attack - 
+        # defense - 
+    # grunt:
+        # health - 5
+        # speed - 
+        # attack - 
+        # defense - 
+        # magic - 10
+    # Don:
+        # health - 10
+        # speed - 
+        # attack - 
+        # defense - 
+        # magic - 15
+
+# ITEMS/INVENTORY:
+    # inventory: [begins empty]
+    # broken wand: True (not in inventory)
+    # cursed lasagne: True (not in inventory)
+    # 30 dollars: True (not in inventory)
+
+# FUNCTIONS:
+    # The Dons office:
+    # Front room:
+    # Incantation room:
+    # Generator room:
+    # Kitchen:
+    # Game room:
+    # The vault:
+    # Meeting room:
+    # Supplies room:
+
+    # Battle:
+    # Player turn:
+    # Enemy turn:
+
+# GAME:
+    # while user wants to play the game:
+        # begin user in the front room, run the Front room function
+        # 
+
+
+
+    
