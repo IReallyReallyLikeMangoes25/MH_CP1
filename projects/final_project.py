86498b8e79daf233d12859a82e8312a2348633c1
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-12-08 10:49:23 -0700

    working on final project code

--- projects/final_project.py
+++ projects/final_project.py
@@ -1,196 +1,100 @@
 # MH 2nd final project
-# Inport the random number generator library
-# INPUT play: ask user "Do you want to play or quit?"
 
-# ROOM CHOICE: none (this will change)
+import random
+play_or_quit = input("Do you want to play or quit?").lower().strip().capitalize()
 
-# Player = True (alive)
+current_room = 0
 
-# STATS:{
-    # player{
-        # health 
-        # speed 
-        # attack 
-        # defense 
-    #}
-    # grunt{
-        # health 
-        # speed 
-        # attack 
-        # defense 
-        # magic 
-    #}
-    # Don{
-        # health
-        # speed
-        # attack
-        # defense
-        # magic
-    #}}
+stats = {
+    "player" : {
+        "health" : 10,
+        "attack" : 13,
+        "special_attack" : 15,
+        "defense" : 7,
+        "special defense" : 6,
+        "speed" : 10
+    },
+    "grunt" : {
+        "health" : 10,
+        "attack" : 7,
+        "defense" : 13,
+        "special defense" : 10,
+        "speed" : 10,
+        "magic" : 10
+    },
+    "Don" : {
+        "health" : 13,
+        "attack" : 7,
+        "defense" : 7,
+        "special defense" : 9,
+        "speed" : 10,
+        "magic" : 17
+    }
+}
 
-# ITEMS/INVENTORY:
-    # inventory: {begins empty}
-    # broken wand: True (not in inventory)
-    # cursed lasagne: True (not eaten)
-    # 30 dollars: True (not in inventory)
-    # Generator: True (power on)
-    # cauldron: True (full)
-    # artifact: True (safe)
+inventory = {
 
-# FUNCTIONS:
-    # ROOM FUNCTIONS:
-    # The Dons office:
-        # In this room the battle function will be run on the Don and the Player
-        # if the player wins print that they won and ask if they want to play again
-        # If the player loses print that they lost and ask if they want to play again
-        # If they want to play again return True, if they want to quit return False
+}
 
-    # Front room:
-        # In this room the player has their choice of two doors
-        # door 1 - leads to the kitchen
-        # door 2 - leads to the supplies room
-        # return their room choice
+player = True
+Don = True
+broken_wand = True
+cursed_lasagne = True
+thirty_dollars = True
+generator = True
+cauldron = True
+artifact = True
 
-    # Incantation room: Takes in cauldron
-        # In this room there's a grunt, run the battle function
-        # in this room if the cauldron is still full ask the player if they would like to use it
-        # if they do randomize to choose what they get - 
-        # +5 attack/speed/defense
-        # return cauldron as False (empty) and display door options
-        # the player has 3 door options in this room
-        # door 1 - Go back to the generator room
-        # door 2 - Go to the meeting room
-        # door 3 - Go to the Dons office
-        # also return their room choice
+def dons_office():
+    pass
 
-    # Generator room: Takes in generator
-        # in this room if the player hasn't already turned off the generator they will be given the choice to turn it off
-        # if they choose to turn it off give all enemies -3 speed and player -3 attack and return generator as False (turned off) and display door options
-        # the player has two door options in this room
-        # door 1 - Go back to the kitchen
-        # door 2 - Go to the incantation room
-        # also return their room choice
+def front_room():
+    pass
 
-    # Kitchen: Takes in cursed lasagne
-        # check if cursed lasagne has been eaten, if it has provide door options, if it hasn't ask player if they want to eat the lasagne
-        # if the player eats the lasagne randomly choose between giving them a stat boost or debuff and return cured lasagne as False (eaten) and display door options
-        # the player will be given three door choices in this room
-        # door 1 - Go back to the front room
-        # door 2 - Go to the generator room
-        # door 3 - Go to the meeting room
-        # also return their room choice
+def kitchen():
+    pass
 
-    # Game room: Takes in broken wand
-        # Check if the broken wand is already in the players inventory, if it is provide door options, if it's not ask the player if they would like to pick up the broken wand
-        # If they want to pick it up return broken wand as False (in inventory) and display door options
-        # The player will have 3 door options in this room:
-        # door 1 - Go to the meeting room
-        # door 2 - Go to the Dons office
-        # door 3 - Go to the vault
-        # also return their room choice
+def supplies_room():
+    pass
 
-    # The vault: Takes in 30 dollars
-        # Check if the $30 is already in the players inventory, if it is provide door options, if it's not ask them if they would like to pick it up
-        # if they pick it up return 30 dollars as False (in inventory) and display door options
-        # in this room there are 2 door options
-        # door 1 - Go to the supplies room
-        # door 2 - Go to the game room
-        # also return their room choice
+def incantation_room():
+    pass
 
-    # Meeting room:
-        # there are two grunts in this room, run the battle function for both
-        # the player has to defeat both to get the door options
-        # the player has 4 door options in this room
-        # door 1 - Go to the kitchen
-        # door 2 - Go to the supplies room
-        # door 3 - Go to the game room
-        # door 4 - Go to the incantation room
-        # return their room choice
+def generator_room():
+    pass
 
-    # Supplies room: Takes in artifact
-        # check if the artifact has already been knocked over, if it hasn't ask the player if they want to knock it over
-        # If they say they do, -2 from all enemy attack stats, return artifact as False (knocked over) and display door options
-        # in this room the player has the choice between 3 doors
-        # door 1 - Go back to the front room
-        # door 2 - Go to the vault
-        # door 3 - Go to the meeting room
-        # also return their room choice
+def game_room():
+    pass
 
-    # BATTLE FUNCTION:
-        # if the player is faster
-            # While the player and enemy are alive:
-                # run player turn function (players + enemies stats/inventory)
-                # run enemy turn function(players + enemies stats)
-            # after someone has died return who won
+def vault():
+    pass
 
-        # if the enemy is faster:
-            # While the player and enemy are alive:
-                # run enemy turn function (players + enemies stats)
-                # run player turn function (players + enemies stats/inventory)
-        # after someone has died return who won
+def meting_room():
+    pass
 
-    # PLAYER TURN FUNCTION: Takes in players stats, enemies stats, and player inventory
-        # ask user what they want to do from the options:
-            # INPUT: "What would you like to do? 1. Attack, 2. Special Attack, 3. Heal, 4. Use inventory item"
-            # if user says "attack":
-                # damage dealt = (attack / 5) - (enemy defense / 5)
-                # return damage dealt
-            # if user says "special attack":
-                # damage dealt = (special attack / 5) - (enemy special defense / 5)
-                # return damage dealt
-            # if user says "heal":
-                # health recovered = random number from 1 - health stat
-                # return health recovered
-            # if user says "Use inventory item"
-                # LOOP:
-                    # for every item in inventory display:
-                        # what number in the inventory this item is
-                        # item name
-                    # INPUT: "What item would you like to use?"
-                    # if the item they want to use is the broken wand:
-                        # randomize a number from 1- 10
-                        # if the number is 5 or higher add 5 points to the players health
-                        # if the number is 4 or lower take five from the players health
-                        # reutrn health
-                    # if the item is the 30 dollars:
-                        # check to see if the enemy is a grunt
-                        # if the enemy is a grunt return that the user won
-                        # if the enemy is the Don take 5 health from the user and return health
+def battle():
+    pass
 
-    # ENEMY TURN FUNCTION: Takes in players stats and enemies stats
-        # generate a random number from 1-4
-        # if the number is 1:
-            # the enemy does a regular attack:
-                # damage to player = (enemy attack / 5) - (player defense / 5)
-                # return damage
-        # in the number is 2:
-            # the enemy does a special attack:
-                # damage to player = (enemy special attack / 5) - (player special defense / 5)
-                # return damage
-        # if the number is 3:
-            # the enemy heals a random number from 1 - enemy health stat
-            # return health
-        # if the number is 4:
-            # the enemy uses a magic attack:
-                # damage = (enemy magic / 5) - (player defense / 5)
-                # reutrn damage
-
-# GAME:
-    # while user wants to play the game:
-        # run front room function and save next room choice in the room choice varaible
-            # after the player has made their initial choice, keep running while the player and Don are alive:
-                # if room choice = front room run front room and update room choice to whatever they choose in this room
-                # else if room choice = kitchen run kitchen
-                # else if room choice = supplies room run supplies room
-                # else if room choice = generator room run generator room
-                # else if room choice = incantation room run incantation room
-                # else if room choice = meeting room run meeting room
-                # else if room choice = game room run game room
-                # else if room choice = vault run vault
-                # else room choice = Dons office run Dons office
-
-        # after either the player or Don dies, as the user if they want to play again
-        # INPUT: "DO you want to play again?"
-        # if they don't change wants to play to false
-        # if they do keep running the loopp
-    
+def player_turn(player_stats, enemy_stats, player_inventory):
+    action = input("What would you like to do?\n1. Attack\n2. Heal\n3. Special attack\n4. Use inventory item")
+    while True:
+        if action == 1:
+            pass
+        elif action == 2:
+            pass
+        elif action == 3:
+            pass
+        elif action == 4:
+            pass
+        else:
+            pass
+def enemy_turn(player_stats, enemy_stats):
+    action = random.randint(1, 4)
+    if action == 1:
+        pass
+    elif action == 2:
+        pass
+    elif action == 3:
+        pass
+    else:
+        pass
