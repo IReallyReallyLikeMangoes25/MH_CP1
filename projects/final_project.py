7efc5b9e81c9263744f9179db793db28b9f61c52
IReallyReallyLikeMangoes25 <mirai.hsiao@ucas-edu.net>
2025-12-09 10:50:38 -0700

    working on coding the final

--- projects/final_project.py
+++ projects/final_project.py
@@ -49,10 +49,19 @@ def dons_office():
     pass
 
 def front_room():
-    pass
+    print("description")
+    choice = input("You see two doors at the back, seems they're the only ways forward. Which way should you go?\nDoor one\nDoor two")
+    while True:
+        if choice == "Door one":
+            print("You walk up to the door and open it, hoping that red stain on the bottom is just marinara.")
+        elif choice == "Door two":
+            print("You approach the door and open it.")
+        else:
+            print("You can't see a door like that anywhere, choose from the ones you can see.")
+            choice = input("You see two doors at the back, seems they're the only ways forward. Which way should you go?\nDoor one\nDoor two")
 
 def kitchen():
-    pass
+    print("Looks like this is just the kitchen")
 
 def supplies_room():
     pass
@@ -75,26 +84,55 @@ def meting_room():
 def battle():
     pass
 
-def player_turn(player_stats, enemy_stats, player_inventory):
+def player_turn(player_attack, player_specialatk, enemy_defense, enemy_specialdef, player_inventory):
     action = input("What would you like to do?\n1. Attack\n2. Heal\n3. Special attack\n4. Use inventory item")
+    auto_win = False
     while True:
         if action == 1:
-            pass
+            damage = (player_attack / 5) - (enemy_defense /5)
+            return damage
         elif action == 2:
-            pass
+            healed = random.randint(0, 4)
+            return healed
         elif action == 3:
-            pass
+            damage = (player_specialatk / 5) - (enemy_specialdef / 5)
         elif action == 4:
-            pass
+            print(player_inventory)
+            use = input("what item would you like to use?")
+            while True:
+                if use == "broken wand":
+                    rand = random.randint(1,2)
+                    if rand == 1:
+                        healed = 5
+                        return healed
+                    else:
+                        healed = -4
+                        return healed
+                elif use == "30 dollars":
+                    if enemy_defense == 13:
+                        auto_win = True
+                        return auto_win
+                    else:
+                        healed = -5
+                        return healed
+                else:
+                    print("That is not in your inventoy, please choose again.")
+                    use = input("what item would you like to use?")
         else:
-            pass
-def enemy_turn(player_stats, enemy_stats):
+            print("That is not an option right now, make anoter choice.")
+            action = input("What would you like to do?\n1. Attack\n2. Heal\n3. Special attack\n4. Use inventory item")
+
+def enemy_turn(player_attack, player_specialdef, player_defense, enemy_attack, enemy_defense, enemy_specialatk, magic):
     action = random.randint(1, 4)
     if action == 1:
-        pass
+        damage = (enemy_attack / 5) - (player_defense /5)
+        return damage
     elif action == 2:
-        pass
+        healed = random.randint(1,4)
+        return healed
     elif action == 3:
-        pass
+        damage = (magic / 5) - (player_specialdef /5)
+        return damage
     else:
-        pass
+        damage = (enemy_specialatk / 5) - (player_specialdef /5)
+        return damage
